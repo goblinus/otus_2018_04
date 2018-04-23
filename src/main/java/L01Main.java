174d6aec5dad5d730bb097b046ca9a975de36fb9
Sergey Tristan <sergey.tristan@gmail.com>
2018-04-24 02:49:25 +0300

    Проект к 1-му уроку

---
+++ src/main/java/L01Main.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * Курс Otus: разработчик Java, 2018, апрель. Домашнее задание по уроку #1
+ *
+ * @author sergey.tristan@gmail.com
+ * @version 1.0
+ */
+public class L01Main {
+    public static void main (String... args) {
+        System.out.println(args.length);
+    }
+}
