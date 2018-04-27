cf6665d180dd72e143ce910c03047add35700381
Sergey Tristan <sergey.tristan@gmail.com>
2018-04-27 22:57:40 +0300

    master Фиксируем структуру проекта

---
+++ 01/src/ru/otus/Obfuscation.java
@@ -0,0 +1,8 @@
+package ru.otus;
+
+public class Obfuscation {
+    static public void main(String[] args) {
+        int length = args.length;
+        System.out.printf("===> %s\n", length);
+    }
+}
---
+++ 02/src/ru/otus/ByteCodeExample.java
@@ -0,0 +1,23 @@
+package ru.otus;
+
+public class ByteCodeExample {
+    private final String name;
+
+    public ByteCodeExample(String name) {
+        this.name = name;
+    }
+
+    public static void main (String []args) {
+        String name = "Tully";
+        ByteCodeExample example = new ByteCodeExample(name);
+        long size = example.getAnswer();
+        System.out.println("The answer for " + example.name + " is: " + size);
+    }
+
+    public long getAnswer() {
+        int a = 1;
+        int b = 5;
+        int c = a << b;
+        return c + 10;
+    }
+}
